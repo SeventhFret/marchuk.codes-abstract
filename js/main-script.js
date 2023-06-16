b489a7b068a5c3498986aaec75431ad310c0f309
Marchuk Maxim <maxxik007@gmail.com>
2023-06-16 19:04:54 +0200

    Finished avatar animation. Working on content.

--- js/main-script.js
+++ js/main-script.js
@@ -8,15 +8,32 @@ else {
 }
 
 
+var headerBlock = document.getElementById('headerBlock');
 var headerText = document.getElementById('headerText');
 var avatar = document.getElementById('avatarPic');
-var headerBlock = document.getElementById('headerBlock');
 
 var avatarAppear = anime({
     autoplay: false,
     targets: '.header-block-avatar',
     duration: 2000,
+    easing: 'easeInOutQuad',
     opacity: 1
+})
+
+var avatarToRightAnimation = anime({
+    autoplay: false,
+    targets: '.header-block-avatar',
+    duration: 2500,
+    translateX: [0, '15vw'],
+    marginLeft: [0, '10vw'],
+})
+
+var headersToLeftAnimation = anime({
+    autoplay: false,
+    targets: '.header-block-text',
+    duration: 2500,
+    translateX: [0, '-15vw'],
+    marginRight: [0, '10vw']
 })
 
 // ? Disable/Enable Scroll
@@ -30,3 +47,10 @@ function enableScroll() {
     document.body.style.overflow = '';
     document.body.style.overflowX = 'hidden';
   }
+
+avatarAppear.play();
+avatarAppear.finished.then(function() {
+    headerText.style.display = 'flex';
+    avatarToRightAnimation.play();
+    headersToLeftAnimation.play();
+})
