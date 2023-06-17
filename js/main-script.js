3894037a1178821c44b48fb5ccc1cee80ae781d9
Marchuk Maxim <maxxik007@gmail.com>
2023-06-17 16:56:04 +0200

    Working on content and its design. Finished education section, working on experience section. Finished animations on scroll for existing elements

--- js/main-script.js
+++ js/main-script.js
@@ -36,6 +36,8 @@ var headersToLeftAnimation = anime({
     marginRight: [0, '10vw']
 })
 
+
+
 // ? Disable/Enable Scroll
 function disableScroll() {
     document.documentElement.style.overflow = 'hidden';
@@ -48,9 +50,81 @@ function enableScroll() {
     document.body.style.overflowX = 'hidden';
   }
 
-avatarAppear.play();
-avatarAppear.finished.then(function() {
-    headerText.style.display = 'flex';
-    avatarToRightAnimation.play();
-    headersToLeftAnimation.play();
+function restoreBlocks() {
+    headerBlock.style.display = 'static';
+    avatar.style.display = 'static';
+}
+
+function playAvatarAnimation() {
+    // disableScroll();
+    avatarAppear.play();
+    avatarAppear.finished.then(function() {
+        headerText.style.display = 'flex';
+        avatarToRightAnimation.play();
+        headersToLeftAnimation.play();
+    })
+    headersToLeftAnimation.finished.then(function() {
+        enableScroll();
+        restoreBlocks();
+    })
+}
+
+playAvatarAnimation();
+
+// Scroll Animation
+const controller = new ScrollMagic.Controller();
+
+var tweenAbout = TweenMax.to("#aboutBlock", 0.7, {css: {opacity: 1, rotationZ: 0}, ease: Power2.easeInOut});
+
+const sceneAbout = new ScrollMagic.Scene({
+    triggerElement: '.about-section',
+    triggerHook: 0.3,
+    reverse: true,
 })
+    .setTween(tweenAbout)
+    .addTo(controller);
+
+
+var tweenEducationSection = TweenMax.to('#educationSection', 0.7, {css: {opacity: 1}, ease: Power2.easeInOut})
+
+const sceneEducationSection = new ScrollMagic.Scene({
+    triggerElement: '.education-section',
+    triggerHook: 0.9,
+    reverse: true
+})
+
+    .setTween(tweenEducationSection)
+    .addTo(controller);
+
+var tweenEducation = TweenMax.to('#educationBlock', 0.7, {css: {opacity: 1, translateX: 0}, ease: Power2.easeInOut})
+
+const sceneEducation = new ScrollMagic.Scene({
+    triggerElement: '.education-section',
+    triggerHook: 0.3,
+    reverse: true
+})
+
+    .setTween(tweenEducation)
+    .addTo(controller);
+    
+var tweenEducationCollege = TweenMax.to('#educationCollege', 0.7, {css: {opacity: 1, translateX: 0}, ease: Power2.easeInOut})
+
+const sceneEducationColege = new ScrollMagic.Scene({
+    triggerElement: '.education-dci',
+    triggerHook: 0.9,
+    reverse: true
+})
+
+    .setTween(tweenEducationCollege)
+    .addTo(controller);
+
+var tweenEducationDci = TweenMax.to('#educationDci', 0.7, {css: {opacity: 1, translateX: 0}, ease: Power2.easeInOut})
+
+const sceneEducationDci = new ScrollMagic.Scene({
+    triggerElement: '.filler',
+    triggerHook: 0.7,
+    reverse: true
+})
+
+    .setTween(tweenEducationDci)
+    .addTo(controller);
